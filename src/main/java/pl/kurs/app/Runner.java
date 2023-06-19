4a25a93042ee5d711aa39cd6f018b89dadd7e0bc
Sebastian Kieszonkowski <k.seba90@gmail.com>
2023-06-20 00:48:56 +0200

    Sixth commit

--- src/main/java/pl/kurs/app/Runner.java
+++ src/main/java/pl/kurs/app/Runner.java
@@ -1,17 +1,7 @@
 package pl.kurs.app;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import pl.kurs.exception.WrongInputArgumentException;
-import pl.kurs.shapes.models.*;
-import pl.kurs.service.ObjectMapperHolder;
-import pl.kurs.service.ShapeService;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Runner {
-    public static void main(String[] args) throws IOException, WrongInputArgumentException {
+    public static void main(String[] args) {
         System.out.println("Zaliczyłem test");
     }
 }
